391d8ca448f6d8711708ba80c735babfee24f457
Andre Putera Pratama <andreputerap@gmail.com>
2020-09-27 02:06:53 +0700

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Sidebar from '../components/Sidebar';
 import { Container } from 'react-bootstrap';
+import { useChats } from '../context/ChatProvider';
+
+// COMPONENTS
+import Sidebar from '../components/Sidebar';
+import OpenChat from '../components/OpenChat';
 
 const Dashboard = ({ id }) => {
+    const { selectedChat } = useChats()
+
     return (
         <Container className="dashboard-page">
             <Sidebar id={id} />
+            {selectedChat && <OpenChat />}
         </Container>
     );
 };
