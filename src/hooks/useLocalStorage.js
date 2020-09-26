24ae529111217385e822253d3355fdabf72c8839
Andre Putera Pratama <andreputerap@gmail.com>
2020-09-27 03:38:03 +0700

    Update useLocalStorage.js

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-const PREFIX = "whatsdown-"
+export const PREFIX = "whatsdown-"
 
 const UseLocalStorage = (key, initialValue) => {
     const prefixedKey = PREFIX + key
